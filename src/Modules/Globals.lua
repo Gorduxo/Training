cb4b332042e11b48fd6b405973e28fb521394de8
Gorduxo <155206864+Gorduxo@users.noreply.github.com>
2024-01-01 03:20:02 +0000

    Atualizar o Globals.lua

--- src/Modules/Globals.lua
+++ src/Modules/Globals.lua
@@ -3,7 +3,19 @@ local self = {}
 --//Services
 self.Players = game:GetService("Players")
 self.ReplicatedStorage = game:GetService("ReplicatedStorage")
+self.TweenService = game:GetService("TweenService")
+self.RunService = game:GetService("RunService")
 
-print("Olá Filipa")
+--//Functions
+if self.RunSerVice:IsClient() then
+--/Client
 
+eles
+--/Server
+end
+
+--/Wideside
+function self:GetComponent(Component,Args)
+
+end
 return self
